cf29c63779c5e499542517f702c9f47bd2ec4243
GersonESS <gebhsantos@gmail.com>
2025-05-06 14:46:49 -0300

    a

---
+++ 03a/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+const users = [];
+
+
+app.get('/usuarios', (req, res) => {
+
+    res.status(200).json(users);
+   });
+
+app.post('/usuarios', (req, res) => {
+  users.push(req.body);
+  
+  res.status(201).json(req.body);
+  });
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
