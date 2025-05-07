2f81ece5e051c7cf87e80c1a42989c649441bcdd
GersonESS <gebhsantos@gmail.com>
2025-05-06 21:53:04 -0300

    a

--- 04a/server.js
+++ 04a/server.js
@@ -1,28 +1,20 @@
 import express from 'express';
-// import pkg from '@prisma/client';
-
-// const { PrismaClient } = pkg;
-// const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 const users = [];
 
+
 app.get('/usuarios', (req, res) => {
+
     res.status(200).json(users);
-});
+   });
 
-app.post('/usuarios', async (req, res) => {
-    await prisma.user.create({
-        data: {
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age,
-        },
-    });
-
-    res.status(201).json(req.body);
-});
+app.post('/usuarios', (req, res) => {
+  users.push(req.body);
+  
+  res.status(201).json(req.body);
+  });
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
-});
+  });
