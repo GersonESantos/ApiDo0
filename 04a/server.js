06f20fbb016af96408afe5c08293f51cbc625e71
GersonESS <gebhsantos@gmail.com>
2025-05-06 21:30:44 -0300

    Update server.js

--- 04a/server.js
+++ 04a/server.js
@@ -1,28 +1,28 @@
 import express from 'express';
-import { PrismaClient } from './generated/prisma'
+import pkg from '@prisma/client';
 
-const prisma = new PrismaClient()
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 const users = [];
 
-
 app.get('/usuarios', (req, res) => {
-
     res.status(200).json(users);
-   });
+});
 
-app.post('/usuarios', (req, res) => {
-  prisma.user.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  })
-  
-  res.status(201).json(req.body);
-  });
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+        },
+    });
+
+    res.status(201).json(req.body);
+});
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
-  });
+});
